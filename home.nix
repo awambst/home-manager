c995c7d8515a53ef9a9935e38cac9df743a1bf28
Arthur Wambst <arthur.wambst@epita.fr>
2025-08-11 23:55:30 +0200

    fix: added wallpapers in home conf

--- home.nix
+++ home.nix
@@ -54,6 +54,11 @@ in
       recursive = true;
     };
 
+    ".config/wallpapers" = {
+      source = ./dotfiles/wallpapers;
+      recursive = true;
+    };
+
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
