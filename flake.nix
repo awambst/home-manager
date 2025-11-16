d3bdc8c80e2082124952034de86bdb9cd99b263d
Arthur Wambst <wambst.arthur2@gmail.com>
2025-11-16 19:28:24 +0100

    try to add firefox

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
+          ./modules/firefox.nix
           ./home.nix
         ];
 
