6cc56b5d14295aac7af9ee990179f1c307676136
Arthur Wambst <arthur.wambst@epita.fr>
2025-12-24 05:13:11 +0100

    more walls and fixes

--- home.nix
+++ home.nix
@@ -94,12 +94,6 @@ in
     kdePackages.kio-extras
   ];
 
-  qt = {
-    enable = true;
-    platformTheme = "kde";
-    style = { name = "oxygen"; };
-  };
-
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
